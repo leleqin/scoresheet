c4653c8167997908c1468d577783eb893444bd42
leleqin <602568224@qq.com>
2018-08-06 11:37:23 +0800

    finish

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,14 +2,21 @@ let sinon = require("sinon");
 let main = require("../lib/main");
 
 describe('main()', () => {
-
-    it('should display main menu once started', () => {
+    it('should display the added student indormation',function(){
         sinon.spy(console, 'log');
-        main();
-        expect(console.log.args.join()).toBe(`1. 添加学生
-2. 生成成绩单
-3. 退出
-请输入你的选择（1～3）：`);
+        let input = "hujian,2015,han,1,数学:80,语文:80,英语:80,编程:80";
+        main.stdin1(input);
+        expect(console.log).toEqual(`
+        { name: 'hujian',
+          code: '2015',
+          nation: 'han',
+          klass: '1',
+          scores:
+           [ { course: '数学', score: 80 },
+             { course: '语文', score: 80 },
+             { course: '英语', score: 80 },
+             { course: '编程', score: 80 } ],
+          sumScore: 320 }`);
     });
 
 });
